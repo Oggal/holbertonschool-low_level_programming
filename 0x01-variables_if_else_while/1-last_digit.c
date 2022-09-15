28ba124325931a91ec4af7ac2f9acfef77234127
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 09:22:18 -0700

    Add file for task 1
    This should fail the checker, despite not being wrong.
    Checker wants the last digit to be the same sign(pos/neg) as the source
    number.

---
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -0,0 +1,26 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - entry function
+ *
+ *Return: 0-Standard Return
+ */
+int main(void)
+{
+	int n;
+	int lastD;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	lastD = abs(n % 10);
+	if (lastD == 0)
+		printf("Last digit of %i is %i and is 0\n", n, lastD);
+	else if (n < 6)
+		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastD);
+	else
+		printf("Last digit of %i is %i and is greater than 5\n", n, lastD);
+	/* Space before Return */
+	return (0);
+}
