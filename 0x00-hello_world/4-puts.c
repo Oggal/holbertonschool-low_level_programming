f57d1d45f5843ea0c06262ba166bfe41cb82a1d3
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 08:11:36 -0700

    Add File for Task 4

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/*
+ * Simple Test C file
+ */
+
+
+
+/**
+ * main - The main function
+ *
+ * Return: 0 -Simple Return
+ */
+int main(void)
+{
+	char[52] str;
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
+	puts(str);
+
+	return (0);
+}
