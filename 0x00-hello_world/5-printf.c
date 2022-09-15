0231957367f6eb9945ad45e4307a3205a4ade8f8
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 08:19:36 -0700

    Add Files for Task 5

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/*
+ * Simple Test C file
+ */
+
+
+
+/**
+ * main - The main function
+ *
+ * Return: 0 -Simple Return
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art\n");
+
+	return (0);
+}
