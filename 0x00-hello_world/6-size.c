dbbbca4dad3b6b91b07fa3c5332efe943ac2d750
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 08:28:19 -0700

    Add Files for Task 6

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/*
+ * Simple Test C file
+ */
+
+
+
+/**
+ * main - The main function
+ *
+ * Return: 0 -Simple Return
+ */
+int main(void)
+{
+	printf("Size of a char: %i byte(s)\n", (int)sizeof(char));
+	printf("Size of a int: %i byte(s)\n", (int)sizeof(int));
+	printf("Size of a long int: %i byte(s)\n", (int)sizeof(long));
+	printf("Size of a long long int: %i byte(s)\n", (int)sizeof(long long));
+	printf("Size of a float: %i byte(s)\n", (int)sizeof(float));
+
+	return (0);
+}
