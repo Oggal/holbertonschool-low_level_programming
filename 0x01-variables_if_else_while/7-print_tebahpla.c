4b667709eefb865d3f893f5dbaba114d5783ffc7
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 11:18:57 -0700

    Add file for Task 7

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ *main - entry function
+ *
+ *Return: 0-Standard Return
+ */
+int main(void)
+{
+	char out = ('z' + 1);
+
+	while (out-- > 'a')
+		putchar(out);
+	putchar('\n');
+	/* Space before Return */
+	return (0);
+}
