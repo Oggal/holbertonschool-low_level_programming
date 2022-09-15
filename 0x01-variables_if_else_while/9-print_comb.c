03c451d91cca1a278798e9f6f3face53d77a8f91
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 11:32:41 -0700

    Add file for Task 9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ *main - entry function
+ *
+ *Return: 0-Standard Return
+ */
+int main(void)
+{
+	char out = ('0' - 1);
+
+	while (out++ < '9')
+	{
+		putchar(out);
+		if (out == '9')
+			break;
+		putchar(',');
+		putchar(' ');
+	}
+	putchar('\n');
+	/* Space before Return */
+	return (0);
+}
