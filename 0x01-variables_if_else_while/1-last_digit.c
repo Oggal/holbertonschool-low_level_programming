1a48ffcfab99128d3e8520e106c2ec8aff6012c1
Philip <Philip.dan.taylor@gmail.com>
2022-09-15 09:25:47 -0700

    Correct Logic in Task 1
    Was comparing N to 6 not lastDigit

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -17,7 +17,7 @@ int main(void)
 	lastD = abs(n % 10);
 	if (lastD == 0)
 		printf("Last digit of %i is %i and is 0\n", n, lastD);
-	else if (n < 6)
+	else if (lastD < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, lastD);
 	else
 		printf("Last digit of %i is %i and is greater than 5\n", n, lastD);
