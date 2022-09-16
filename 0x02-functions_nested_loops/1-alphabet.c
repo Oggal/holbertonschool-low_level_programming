ae73203f8d459dd7a7117aa197d19ca6a8d4f4dd
Philip <Philip.dan.taylor@gmail.com>
2022-09-16 08:59:25 -0700

    Add Files for Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,20 @@
+/************************
+ *
+ *   Alphabet Game
+ * Because I don't like empty heads
+ *
+ ***********************/
+#include "main.h"
+
+/**
+ *print_alphabet - Print out the alphabet in lowercase
+ *Return: void
+ */
+void print_alphabet()
+{
+	char i = 'a';
+	do
+	{
+		_putchar(i);
+	}while(i < 'z');
+}
