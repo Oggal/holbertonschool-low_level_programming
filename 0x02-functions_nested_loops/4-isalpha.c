06992a3a8626bf285ec833e38c548e7ef58e7d48
Philip <Philip.dan.taylor@gmail.com>
2022-09-16 11:38:10 -0700

    Add Files for task 4

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_isalpha - check is a char is letter
+ *@c: int, char to check
+ *
+ *Return: 0 not a leter
+ *         1 is a leter
+ */
+int _isalpha(int c)
+{
+	if (c > 'A' && c < 'Z' || c > 'a' && c < 'z')
+	{
+		return (1);
+	}
+	return (0);
+}
