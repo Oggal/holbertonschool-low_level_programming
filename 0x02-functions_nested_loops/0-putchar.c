f20b5ebaef84a6d4898310d8d8739f3c84bde7ed
Philip <Philip.dan.taylor@gmail.com>
2022-09-16 08:10:08 -0700

    Correct for pre-C99

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,8 +9,9 @@ int main(void)
 {
 	char str[] = "_putchar";
 	int count = sizeof(str) / sizeof(str[0]);
+	int i;
 
-	for (int i = 0; i < count; i++)
+	for (i = 0; i < count; i++)
 	{
 	_putchar(str[i]);
 	}
