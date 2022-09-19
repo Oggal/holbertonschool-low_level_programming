52b51920e3a75721ab315ea6da7e81df336f13d5
Philip <Philip.dan.taylor@gmail.com>
2022-09-19 08:07:50 -0700

    Correct Task 11 for Betty Style

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -2,19 +2,19 @@
 #include <stdio.h>
 
 /**
- *print_to_98
+ *print_to_98 - Print numbers from @start to 98
  *@start:Starting point on our way to 98
  *Return: void
  */
 void print_to_98(int start)
 {
-	printf(start);
+	printf("%i", start);
 	while (start != 98)
 	{
 		if (start > 98)
 			start--;
 		else
 			start++;
-		printf(", %i",start);
+		printf(", %i", start);
 	}
 }
