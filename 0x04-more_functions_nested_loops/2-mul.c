f20316b1e732702066316195dbb46c2ec67e6a8f
Philip <Philip.dan.taylor@gmail.com>
2022-09-22 07:51:04 -0700

    Add Files for Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *mul - Multiply 2 numebers
+ * NOTE :: We don't handle overflow here
+ *@a: operand A
+ *@b: operand B
+ *
+ *Return: A*B
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
