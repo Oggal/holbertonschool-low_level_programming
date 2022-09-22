5305845fdd931a83c03bfac51c067141542bd293
Philip <Philip.dan.taylor@gmail.com>
2022-09-22 11:39:55 -0700

    Add Files for task 10

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ *print_triangle - Print a # Triangle
+ *@size: Size of Triangle
+ *
+ */
+void print_triangle(int size)
+{
+	int i, j;
+
+	if ( size <= 0)
+	{
+		_putchar('\n');
+		return;
+	}
+        for (i = 0; i < size; i++)
+	{
+		for (j = (size-1); j >=0; j--)
+		{
+			if (j > i)
+				_putchar(' ');
+			else
+				_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
