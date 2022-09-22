392da7580d4fe1ac260eb7d5cc18f3b1879f2310
Philip <Philip.dan.taylor@gmail.com>
2022-09-22 09:08:25 -0700

    Add Files for Task 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *print_line - Print a line of _
+ *@x: Number of _ to print
+ *
+ */
+void print_line(int x)
+{
+	int i;
+
+	for (i = 0; i < x; i++)
+		_putchar('_');
+	_putchar("\n");
+}
