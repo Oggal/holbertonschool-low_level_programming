3c1858f4cd8c3fc635d87955b88d7b96bc42a7fb
Philip <Philip.dan.taylor@gmail.com>
2022-09-22 09:47:45 -0700

    Correct for C89 vs C99
    I don't want to allocate memory if I won't need it. I want to check and
    return before allocating my vars. C89 doesn't care.

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -7,13 +7,13 @@
  */
 void print_square(int size)
 {
+	int x, y;
+
 	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-	int x, y;
-
 	for (y = 0; y < size; y++)
 	{
 		for (x = 0; x < size; x++)
