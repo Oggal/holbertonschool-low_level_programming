9c5eb569da9151fc5735a98503f5c5f91a88717e
Philip <Philip.dan.taylor@gmail.com>
2022-09-22 07:42:17 -0700

    Add Files for Task 1

---
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _isdigit - Return if char value of c is digit
+ *@c: Value to check
+ *
+ *Return: 1 if digit, otherwise 0
+ */
+int _isdigit(int c)
+{
+        return (0 + (c >= '0' && c <= '9'));
+}
