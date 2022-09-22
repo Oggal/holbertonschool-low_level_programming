0935493259820a8e6a650980d7d589560d9a40f1
Philip <Philip.dan.taylor@gmail.com>
2022-09-22 11:41:43 -0700

    All Hail Queen Betty

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -9,14 +9,14 @@ void print_triangle(int size)
 {
 	int i, j;
 
-	if ( size <= 0)
+	if (size <= 0)
 	{
 		_putchar('\n');
 		return;
 	}
-        for (i = 0; i < size; i++)
+	for (i = 0; i < size; i++)
 	{
-		for (j = (size-1); j >=0; j--)
+		for (j = (size - 1); j >= 0; j--)
 		{
 			if (j > i)
 				_putchar(' ');
