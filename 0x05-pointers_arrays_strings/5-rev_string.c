e2e6181168ec2390c90a0f0765c242ccfa5fff14
Philip <Philip.dan.taylor@gmail.com>
2022-09-23 12:25:46 -0700

    All Glory to Betty!

--- 0x05-pointers_arrays_strings/5-rev_string.c
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -8,14 +8,14 @@
 void rev_string(char *s)
 {
 	int j, i = 0;
-	char old[200];
+	char old[800];
 
 	while ((s[i]) != '\0')
 	{
-		old[i+1] = s[i];
+		old[i + 1] = s[i];
 		i++;
 	}
-        
+
 	for (j = 0; j < (i); j++)
 	{
 		s[j] = old[i - j];
