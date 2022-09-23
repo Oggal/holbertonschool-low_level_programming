ec02d82f4a86f8b2b499e87f36a21e6448189a1c
Philip <Philip.dan.taylor@gmail.com>
2022-09-23 07:18:55 -0700

    Add Files for Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ *_puts - Print a String
+ *@s: String to print
+ *
+ */
+void _puts(char s*)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+		_putchar(s[i++]);
+	_putchar('\n');
+}
--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -7,5 +7,6 @@ int _strlen(char *s);
 
 void swap_int(int *a, int *b);
 void reset_to_98(int *n);
+void _puts(char s*);
 
 #endif
