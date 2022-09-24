9a69c6d0265da0a788f984780845c2b8219ede79
Philip <Philip.dan.taylor@gmail.com>
2022-09-24 10:32:56 -0700

    "All Hail Queen Betty!"

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,10 +1,11 @@
 #include "main.h"
 
 /**
- *_strcpy
+ *_strcpy - Copy String to Buffer
  *@dest: Destionation Char Buffer
  *@src:  Source Char Pointer
  *
+ *Return: Pointer to Copied String
  */
 char *_strcpy(char *dest, char *src)
 {
