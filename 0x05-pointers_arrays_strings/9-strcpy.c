959327ee4e92720c4429753493f71a403ba4edcc
Philip <Philip.dan.taylor@gmail.com>
2022-09-24 10:31:54 -0700

    Add Files for Task 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ *_strcpy
+ *@dest: Destionation Char Buffer
+ *@src:  Source Char Pointer
+ *
+ */
+char *_strcpy(char *dest, char *src)
+{
+	int i = 0;
+
+	while (src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+
+	return (dest);
+}
