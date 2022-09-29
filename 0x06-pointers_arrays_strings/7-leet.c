4496446b91344026d320b6c2ece68848f1c263ad
Philip <Philip.dan.taylor@gmail.com>
2022-09-29 12:39:40 -0700

    Add Files for Task 7

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,33 @@
+#include "holberton.h"
+
+/**
+ *leet - m4k3 17 1337sp34k
+ *@s: str1ng t0 m0d
+ *
+ *Return: M0d1f13d Str1ng
+ */
+char *leet(char *s)
+{
+	int l33t[8] = -1;
+	int a2A = 'A' - 'a';
+	int i, j;
+
+	l33t[0] = 'O';
+	l33t[1] = 'L';
+	l33t[3] = 'E';
+	l33t[4] = 'A';
+	l33t[7] = 'T';
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; j < 8; j++)
+		{
+			if ((s[i] + a2A) == l33t[j] || s[i] == l33t[j])
+			{
+				s[i] = '0' + j;
+			}
+		}
+	}
+
+	return (s);
+}
--- 0x06-pointers_arrays_strings/holberton.h
+++ 0x06-pointers_arrays_strings/holberton.h
@@ -8,6 +8,7 @@ char *_strncat(char *dest, char *src, int n);
 char *_strncpy(char *dest, char *src, int n);
 char * string_toupper(char *s);
 char *cap_string(char *s);
+char *leet(char *s);
 
 int _strcmp(char *s1,char *s2);
 
