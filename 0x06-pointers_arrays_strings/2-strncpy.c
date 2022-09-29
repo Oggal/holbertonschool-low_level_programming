6c6a544fbf8b53017433283fd852fc789bea4c34
Philip <Philip.dan.taylor@gmail.com>
2022-09-29 09:12:00 -0700

    Add Files for Task 2

---
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,27 @@
+#include "holberton.h"
+
+/**
+ *_strncpy - Copy a string into dest
+ *@dest: First and Returned String
+ *@src:  String to copy onto the first
+ *@n: Max chars to copy
+ *Return: Pointer to @dest
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int hitEnd = 0, i = 0;
+
+	while (n > i)
+	{
+		if(src[i] && !hitEnd)
+			dest[i] = src[i];
+		else
+		{
+			dest[i] = '\0';
+			hitEnd = 1;
+		}
+		i++;
+	}
+
+       	return (dest);
+}
