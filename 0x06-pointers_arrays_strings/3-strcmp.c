09ca95bf7de97562b4471397d9a1bbcbf6dcbdfc
Philip <Philip.dan.taylor@gmail.com>
2022-09-29 09:51:18 -0700

    Add Files for Task 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+
+/**
+ *_strcmp - Compare 2 Strings
+ *@s1: StringA
+ *@s2: StringB
+ *
+ *Return: Int value diffrence in stringsg
+ */
+int _strcmp(char *s1, char *s2)
+{
+
+	int vA, vB, i;
+
+	for (i = 0; s1[i] && s2[i] && s1[i] == s2[i]; i++)
+		;
+
+	return (s1[i] - s2[i]);
+}
