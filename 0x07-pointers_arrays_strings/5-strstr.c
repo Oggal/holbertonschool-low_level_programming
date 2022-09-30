35d03148b0f27ff9cde3b8e5bebd84d071402253
Philip <Philip.dan.taylor@gmail.com>
2022-09-30 11:05:39 -0700

    Add Files for Task 5

---
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stddef.h>
+
+/**
+ *_strstr - Find a string
+ *@s: String to search
+ *@a: string to find
+ *
+ *Return: Pointer to byte found
+ */
+char *_strpbrk(char *s, char *a)
+{
+	int i, j;
+
+	for (i = 0; s[i]; i++)
+	{
+		for (j = 0; a[j]; j++)
+		{
+			if (s[i+j] != a[j])
+				break;
+		}
+		if (!a[j]) /*Check if loop ended naturally*/
+			return (s + i);
+	}
+
+	return (NULL);
+}
