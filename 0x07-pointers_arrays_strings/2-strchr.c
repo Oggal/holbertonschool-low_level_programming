30e81826c4c1f20a36a158981438cb644c9a9343
Philip <Philip.dan.taylor@gmail.com>
2022-09-30 07:15:58 -0700

    Add Files for Task 2

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strchr - Get pointer to First @c
+ *@s: String to search
+ *@c: Char to find
+ *
+ *Return: Pointer to @c or NULL
+ */
+char *_strchr(char *s, char c)
+{
+	int i = 0;
+
+	while (s[i] && s[i] != c)
+		i++;
+	if (!s[i])
+		return (NULL);
+
+	return (s + i);
+}
--- 0x07-pointers_arrays_strings/main.h
+++ 0x07-pointers_arrays_strings/main.h
@@ -5,5 +5,6 @@ int _putchar(char);
 
 char *_memset(char *s, char b, unsigned int n);
 char *_memcpy(char *des, char *src, unsigned int n);
+char *_strchr(char *s, char c);
 
 #endif
