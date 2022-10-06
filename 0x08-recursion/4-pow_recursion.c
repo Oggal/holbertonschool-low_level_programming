a7fcbfd78efa00570da2244cae8dcf67abd73c54
Philip <Philip.dan.taylor@gmail.com>
2022-10-06 06:43:28 -0700

    Add Files for task 4

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *_pow_recursion - Calulate X^y
+ *@x:base
+ *@y:power
+ *
+ *Return: X ^ Y
+ */
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+		return (-1);
+	if (!y)
+		return (1);
+	return (x * _pow_recursion(x, y - 1));
+}
