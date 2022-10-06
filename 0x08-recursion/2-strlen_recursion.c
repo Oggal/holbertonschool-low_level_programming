9628a32f55b183d31af8c43c9984700702a895bf
Philip <Philip.dan.taylor@gmail.com>
2022-10-06 06:30:03 -0700

    Add Files For Task 2

---
+++ 0x08-recursion/2-strlen_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *_strlen_recursion - Find String Length, 
+ *@s: String to count
+ *
+ * Return: Length of String
+ */
+int _strlen_recursion(char *s)
+{
+	if (!*s)
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
