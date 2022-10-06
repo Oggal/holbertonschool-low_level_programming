97f2e6ae9c6b2923fcecc32487409fa7a90e3b3b
Philip <Philip.dan.taylor@gmail.com>
2022-10-06 13:58:47 -0700

    Add Files for Task 1

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ *main - Program Entry Point
+ *@argc: Argument Count
+ *@argv: Argument Values
+ *
+ *Return: 0, Always
+ */
+int main(int argc, __attribute__((unused))char *argv[])
+{
+	printf("%d\n", argc);
+
+	return (0);
+}
