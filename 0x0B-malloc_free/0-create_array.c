37259e8e113638c3726fb0d007d963177ad40c2f
Philip <Philip.dan.taylor@gmail.com>
2022-10-07 08:29:45 -0700

    Add Files for Project 0x0B, Task 0

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *create_array - Dynamically Create Array
+ *@size: Size of Array to Create
+ *@c: Byte to fill Array with
+ *
+ *Return: Created Array, or null pointer when things go wrong
+ */
+char *create_array(unsigned int size, char c)
+{
+	char *output;
+	int i;
+
+	output = malloc(sizeof(char)*size);
+	if (output == NULL)
+		return (output);
+	for (i = 0; i < size; i++)
+	{
+		output[i] = c;
+	}
+	return (output);
+}
---
+++ 0x0B-malloc_free/main.h
@@ -0,0 +1,10 @@
+#ifndef OGGAL
+#define OGGAL
+
+#include <stddef.h>
+/*Provided Methods*/
+int _putchar(char);
+/*Project Methods*/
+char *create_array(unsigned int size, char c);
+
+#endif
