3cd0a3d84c0b7eee472ad748af07eff99927260f
Philip <Philip.dan.taylor@gmail.com>
2022-10-07 08:51:48 -0700

    Add Files for Task 1

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,30 @@
+#include "main.h"
+
+/**
+ *_strdup - Duplicate a string to a fresh Array
+ *@str: string to dupe
+ *
+ *Return: A pointer to a duplicated String
+ */
+char *_strdup(char *str)
+{
+	char *o; /*Output*/
+	int len, i;
+
+	/*We know str is a string, find the length*/
+	len = 0;
+	while (str[len++])
+		;
+	/*Bam! Length got! with an extra for the null byte*/
+	o = malloc(sizeof(char) * len);
+	if (o == NULL)
+		return (NULL);
+	/*Just checked if we found memmory, going to <= to include
+	  the null byte. This Comment may not pass betty*/
+	for (i = 0; i <= len; i++)
+	{
+		o[i] = str[i];
+	}
+
+	return (o);
+}
