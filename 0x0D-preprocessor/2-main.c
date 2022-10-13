796f5d88e6788e4b58d8975f4b53bd3d3a6c9ef9
Philip <Philip.dan.taylor@gmail.com>
2022-10-13 08:30:42 -0700

    Correct Task 2

--- 0x0D-preprocessor/2-main.c
+++ 0x0D-preprocessor/2-main.c
@@ -1,7 +1,16 @@
 #include <stddef.h>
 #include <stdio.h>
 
+
+
+/**
+ *main - Program Entry
+ *
+ *Return: 0, Always.
+ */
 int main(void)
 {
-	printf("%s\n",__FILE__);
+	printf("%s\n", __FILE__);
+
+	return (0);
 }
