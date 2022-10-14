b0e32d85e25a958fca5fbfee6cb6ab74554643b4
Philip <Philip.dan.taylor@gmail.com>
2022-10-14 15:15:51 -0700

    Add Files for Task 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * array_range - Create an array of integers
+ * @min: Starting point of the array
+ * @max: End point of the array
+ *
+ * Return: New Array starting at min, ending at max. Include Both.
+ */
+int *array_range(int min, int max)
+{
+	int i, range = max - min;
+	int *output;
+
+	if (range < 0)
+		return (NULL);
+	output = malloc(sizeof(int) * (range + 1));
+	if (output == NULL)
+		return (NULL);
+	for (i = 0; i <= range; i++)
+	{
+		output[i] = min + i;
+	}
+
+	return (NULL);
+}
--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -9,6 +9,7 @@ void *_calloc(unsigned int unemb, unsigned int size);
 
 char *string_nconcat(char *s1, char *s2, unsigned int n);
 
+int *array_range(int min, int max);
 int _putchar(char);
 
 #endif
