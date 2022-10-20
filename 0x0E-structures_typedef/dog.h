a36871353c453ca2f73114468954b2c25a7df4ec
Philip <Philip.dan.taylor@gmail.com>
2022-10-20 09:29:21 -0700

    Complete Task 4

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,7 @@
 #ifndef DOG_H
 #define DOG_H
 #include <stddef.h>
-
+#include <stdlib.h>
 /**
  * struct dog - Simple Data object for a dog
  *@name: name of dog
@@ -20,4 +20,7 @@ typedef struct dog dog_t;
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
+
+dog_t *new_dog(char *name, float age, char *owner);
+
 #endif
