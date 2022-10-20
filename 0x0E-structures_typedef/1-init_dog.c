79df6c273d5e0c0fc46d049b57bd15b910002f71
Philip <Philip.dan.taylor@gmail.com>
2022-10-20 07:25:52 -0700

    Add Files for Task 1
    Still need to check betty

---
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -0,0 +1,15 @@
+#include "dog.h"
+
+/**
+ *init_dog - Initialize a dog!
+ *@d: Target dog to be initialized
+ *@name: Name of dog
+ *@age: Age of dog
+ *@owner:Name of dog's owner
+ */
+void init_dog(stuct dog *d,char *name, float age, char *owner)
+{
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
+}
