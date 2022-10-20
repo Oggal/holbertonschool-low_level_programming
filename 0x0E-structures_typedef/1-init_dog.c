b3af105d856e4ba8fd31895b1928b5a090bc8ab6
Philip <Philip.dan.taylor@gmail.com>
2022-10-20 07:29:33 -0700

    Change Styntax

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -9,7 +9,7 @@
  */
 void init_dog(stuct dog *d,char *name, float age, char *owner)
 {
-	d->name = name;
-	d->age = age;
-	d->owner = owner;
+	(*d).name = name;
+	(*d).age = age;
+	(*d).owner = owner;
 }
