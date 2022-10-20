6dec124b3f2578d5afb1eff0f6d62a719202954d
Philip <Philip.dan.taylor@gmail.com>
2022-10-20 09:08:40 -0700

    Add Files for Task 2

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,6 @@
 #ifndef DOG_H
 #define DOG_H
-
+#include <stddef.h>
 
 /**
  * struct dog - Simple Data object for a dog
@@ -17,5 +17,5 @@ struct dog
 };
 
 void init_dog(struct dog *d, char *name, float age, char *owner);
-
+void print_dog(struct dog *d);
 #endif
