6327db710f031629feb51c4ce8e4cd540d2d1f63
Philip <Philip.dan.taylor@gmail.com>
2022-10-21 10:00:52 -0700

    Betty Header

--- 0x0F-function_pointers/3-calc.h
+++ 0x0F-function_pointers/3-calc.h
@@ -17,11 +17,11 @@ typedef struct op
 	int (*f)(int a, int b);
 } op_t;
 
-int op_add(int,int);
-int op_sub(int,int);
-int op_mul(int,int);
-int op_div(int,int);
-int op_mod(int,int);
+int op_add(int, int);
+int op_sub(int, int);
+int op_mul(int, int);
+int op_div(int, int);
+int op_mod(int, int);
 
 int (*get_op_func(char *s))(int, int);
 
