694ae595553d035fde0bf34e723b3e3e2864f285
Oggal <Philip.dan.taylor@gmail.com>
2022-11-04 10:52:55 -0700

    Add Files for Task 5

---
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -0,0 +1,16 @@
+#include "lists.h"
+
+/**
+ * free_listint2 - Free a list of ints
+ * @head: Head of List to free
+ *
+ */
+void free_listint2(listint_t *head)
+{
+	if (head == NULL)
+		return;
+	if (head->next != NULL)
+		free_listint(head->next);
+	free(head);
+	head == NULL;
+}
