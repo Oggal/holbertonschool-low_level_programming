c066407c23b24c4022fbe05fe3e1da12bcf31c77
Oggal <Philip.dan.taylor@gmail.com>
2022-11-08 15:15:40 -0800

    Add Files for Task 0, Project 0x15 1141

---
+++ 0x15-file_io/0-read_textfile.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ *
+ *
+ *
+ *
+ * Return: Letters Printed
+ */
+ssize_t read_textfile(const char *filename, size_t letters)
+{
+	int fileDesc, size, i, c = 0;
+	char *Buff;
+
+	if (filename == NULL)
+		return (0);
+	Buff = malloc(letters);
+	if (Buff == NULL)
+		return (0);
+	fileDesc = open(filename, O_RDONLY);
+	if (fileDesc < 0)
+	{
+		free(Buff);
+		return (0);
+	}
+	size = read(fileDesc, Buff, letters);
+	close(fileDesc);
+	for (i = 0; Buff[i] && i < size; i++)
+	{
+		c += _putchar(Buff[i]);
+	}
+	return (c);
+}
---
+++ 0x15-file_io/main.h
@@ -0,0 +1,12 @@
+#ifndef OGG_M
+#define OGG_M
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+
+int _putchar(char);
+
+ssize_t read_textfile(const char *filename, size_t letters);
+
+#endif
