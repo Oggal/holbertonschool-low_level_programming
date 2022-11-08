d2ea9482fc71a3817ac5356dae6e1b9078604c41
Oggal <Philip.dan.taylor@gmail.com>
2022-11-08 14:46:03 -0800

    Correct Task 5

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -14,9 +14,9 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int mask = 1;
 	unsigned int count = 0;
 
-        while (mask < n || mask < m)
+        while (mask <= n || mask <= m)
 	{
-		count += ((n & mask) == (m & mask));
+		count += !((n & mask) == (m & mask));
 		mask = mask << 1;
 	}
 
