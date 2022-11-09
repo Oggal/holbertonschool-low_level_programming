67e1f8941b6bb7fd7872f42479c305333a342029
Oggal <Philip.dan.taylor@gmail.com>
2022-11-08 16:23:53 -0800

    Add Files for Task 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * create_file - Create a new File
+ *@filename: File to create
+ *@text_content: Contents to Add
+ *
+ *Return: 1 on success, -1 on failure
+ */
+int create_file(const char *filename,char *text_content)
+{
+	int fd, writeResult, len = 0;
+
+	if (filename == NULL)
+		return (-1);
+	while (text_content[len])
+		len++;
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd < 0)
+		return (-1);
+	writeResult = write(fd, text_content, len);
+	close(fd);
+	return (1);
+}
