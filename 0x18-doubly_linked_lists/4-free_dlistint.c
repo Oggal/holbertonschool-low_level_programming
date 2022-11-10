8347a074010a1d78a64259bc5f237950f11bae03
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 11:44:27 -0800

    Correct missing char

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -9,7 +9,7 @@ void free_dlistint(dlistint_t *head)
 	if (head == NULL)
 		return;
 	if (head->prev)
-		free_dlistint(head-prev);
+		free_dlistint(head->prev);
 	if (head->next)
 		free_dlistint(head->next);
 	free(head);
