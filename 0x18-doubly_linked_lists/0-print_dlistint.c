2ad1730288ddd0fe735456208191775c09fda04c
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 08:36:24 -0800

    Add Files for Task 0, Project 0x18 1143

---
+++ 0x18-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ *print_dlistint - Print a doubly linked list
+ *@h: Head of list
+ *
+ * Return: Count of Elements
+ */
+size_t print_dlistint(const dlistint *h)
+{
+	/*
+	 *  Gonna assume we're given the head every time
+	 * Atleast for now...
+	 */
+	if (h == NULL)
+		return (0);
+	printf("%i\n", h->n);
+	return (1 + print_dlistint(h->next));
+}
---
+++ 0x18-doubly_linked_lists/README.md
@@ -0,0 +1,1 @@
+Project 0x18 1143 - doubly_linked_lists
