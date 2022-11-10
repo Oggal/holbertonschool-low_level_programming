8c0b8bf1e0b6418b6d343d9c49c7b2827468f09c
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 12:18:43 -0800

    Add Files for Task 5

---
+++ 0x18-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,24 @@
+#include "lists.h"
+
+/**
+ *get_dnodeint_at_index - Get Node At Index
+ *@head: Head of list
+ *@index: Index of node to get
+ *
+ *Return: Node at index or null
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
+{
+	if (head == NULL)
+		return (NULL);
+	while (head->prev)
+		head = head->prev;
+	for (; head->next; index--, head = head->next)
+	{
+		if (index)
+			continue;
+		else
+			return (head);
+	}
+	return (NULL);
+}
