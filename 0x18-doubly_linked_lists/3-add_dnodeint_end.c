295cd1ca61c1d8eb77e46c7697004b6200d23851
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 10:12:03 -0800

    Correct Previous to Prev

--- 0x18-doubly_linked_lists/3-add_dnodeint_end.c
+++ 0x18-doubly_linked_lists/3-add_dnodeint_end.c
@@ -25,6 +25,6 @@ dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 		*head = (*head)->next;
 	}
 	(*head)->next = newNode;
-	newNode->previous = *head;
+	newNode->prev = *head;
 	return (newNode);
 }
