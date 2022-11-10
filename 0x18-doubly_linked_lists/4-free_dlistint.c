fab00d27e68ba2a6df4361c3e30c3e359b624db4
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 11:55:23 -0800

    Correct Task 4

--- 0x18-doubly_linked_lists/4-free_dlistint.c
+++ 0x18-doubly_linked_lists/4-free_dlistint.c
@@ -6,11 +6,16 @@
  */
 void free_dlistint(dlistint_t *head)
 {
+	dlistint_t *L;
 	if (head == NULL)
 		return;
-	if (head->prev)
-		free_dlistint(head->prev);
-	if (head->next)
-		free_dlistint(head->next);
-	free(head);
+	while (head->prev)
+		head = head->prev;
+	while (L->next)
+	{
+		free(L->prev);
+		L=L->next;
+	}
+	free(L->prev);
+	free(L);
 }
