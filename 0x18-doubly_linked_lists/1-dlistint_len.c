ed3365b5e88557cb42cd4abd74a471f599bac174
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 08:49:00 -0800

    Add Files for Task 1

---
+++ 0x18-doubly_linked_lists/1-dlistint_len.c
@@ -0,0 +1,18 @@
+#include "lists.h"
+
+/**
+ *dlistint_len - Count a doubly linked list
+ *@h: Head of list
+ *
+ * Return: Count of Elements
+ */
+size_t dlistint_len(const dlistint_t *h)
+{
+	/*
+	 *  Gonna assume we're given the head every time
+	 * Atleast for now...
+	 */
+	if (h == NULL)
+		return (0);
+	return (1 + print_dlistint(h->next));
+}
