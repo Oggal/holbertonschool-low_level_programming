ef1b4bda26d7449253dc56648cb7d509ec594e78
Oggal <Philip.dan.taylor@gmail.com>
2022-11-10 09:31:44 -0800

    Add Files for Task 2

---
+++ 0x18-doubly_linked_lists/2-add_dnodeint.c
@@ -0,0 +1,29 @@
+#include "lists.h"
+
+/**
+ *add_dnodeint - Add A node to a dlist
+ *@head: Head of dList
+ *@n: Node Value to Add
+ *
+ * Return: Node, or NULL
+ */
+dlistint_t *add_dnode_int(dlistint_t **head, const int n)
+{
+
+	dlistint_t *newNode;
+
+	if (head == NULL)
+		return (NULL);
+	newNode = malloc(sizeof(dlistint_t));
+	if ( newNode == NULL)
+		return (NULL);
+	newNode->n  = n;
+	if (*head == NULL)
+		return (*head = newNode);
+	while((*head)->prev)
+	{
+		*head = (*head)->prev;
+	}
+	(*head)->prev = *head;
+	return (*head = newNode);
+}
