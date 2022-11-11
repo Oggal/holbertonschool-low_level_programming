a2d7ee79afa3c4feac9f687f75152b512a7767fa
Oggal <Philip.dan.taylor@gmail.com>
2022-11-11 08:16:24 -0800

    Correct Task 8

--- 0x18-doubly_linked_lists/8-delete_dnodeint.c
+++ 0x18-doubly_linked_lists/8-delete_dnodeint.c
@@ -7,21 +7,31 @@
  *
  *Return: Node at index or null
  */
+
 int delete_dnodeint_at_index(dlistint_t **h, unsigned int index)
 {
 	dlistint_t *head;
 
-	head = get_dnodeint_at_index(*h,index);
-	if (!head)
+	if (h == NULL)
 		return (-1);
-	if (index == 0)
-		h = &(head->next);
-	if ((head)->next)
-		((head)->next)->prev = (head)->prev;
+	head = *h;
+	while (head->prev)
+		head = head->prev;
+	for (; head->next; index--, head = head->next)
+	{
+		if (index)
+			continue;
+		else
+			break;
+	}
+	if (index)
+		return (-1);
 	if (head->prev)
 		head->prev->next = head->next;
 	else
 		*h = head->next;
+	if ((head)->next)
+		((head)->next)->prev = (head)->prev;
 	free(head);
 	return (1);
 }
