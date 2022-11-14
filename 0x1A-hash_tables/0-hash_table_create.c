5c962de2893acce4058cc5a4fad02b22c4cd6a34
Oggal <Philip.dan.taylor@gmail.com>
2022-11-14 06:47:32 -0800

    Correct Malloc and Betty

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -13,13 +13,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 
 	if (size == 0)
-	    return (NULL);
+		return (NULL);
 	ht = malloc(sizeof(hash_table_t));
 	if (ht == NULL)
 		return (NULL);
 
 	ht->size = size;
-	ht->array = malloc(sizeof(hash_node_t) * size);
+	ht->array = malloc(sizeof(hash_node_t *) * size);
 	if (ht->array == NULL)
 	{
 		free(ht);
