1a7eda4d49795c8de15df26ce887bb6a925b596a
Oggal <Philip.dan.taylor@gmail.com>
2022-11-14 10:22:01 -0800

    Add Files For Task 3

---
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -0,0 +1,31 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_set - Set Value for Key in table
+ * @ht: Table to store value in
+ * @key: Key to value
+ * @value: Value to store
+ *
+ * Return : 1 on success
+ */
+int hash_table_set(hash_table_t *ht, const char *key, const char *value)
+{
+	unsigned int index = 0;
+	hash_node_t *listHead, *newNode;
+
+	if (ht == NULL || key == NULL || *key == '\0')
+		return (0);
+	index = key_index(key, ht->size);
+        newNode = malloc(sizeof(hash_node_t));
+	if (newNode == NULL)
+		return (0);
+	newNode->key = strdup(key);
+	newNode->value = strdup(value);
+        newNode->next = NULL;
+
+	listHead = ht->array[index];
+	while (listHead)
+		listHead = listHead->next;
+        listHead = newNode;
+	return (1);
+}
