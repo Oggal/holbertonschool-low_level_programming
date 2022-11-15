4d44291f35bcb847ff231ef34b35cc16c22cd4d4
Oggal <Philip.dan.taylor@gmail.com>
2022-11-15 11:06:35 -0800

    Add Files for task 6

---
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -0,0 +1,27 @@
+#include "hash_tables.h"
+
+/**
+ * hash_table_delete - free a hash table
+ * @ht: hash Table to free;
+ */
+void hash_table_delete(hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *node, *last;
+	if (ht == NULL)
+		return;
+	for (i = 0; i < ht->size; i++)
+	{
+		node = ht->array[i];
+		while (node)
+		{
+			last = node;
+			node = node->next;
+			free(last->key);
+			free(last->value);
+			free(last);
+		}
+	}
+	free(ht->array);
+	free(ht);
+}
