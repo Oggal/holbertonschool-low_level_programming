0fbe0c09f082e82553c1db17e9836059a1061f64
Oggal <Philip.dan.taylor@gmail.com>
2022-11-15 10:01:24 -0800

    Correct Task 5

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -6,7 +6,8 @@
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
-	hash_node_t *currNode;
+	hash_node_t *currNode = NULL;
+	int counter = 0;
 
 	if (ht == NULL)
 		return;
@@ -14,14 +15,15 @@ void hash_table_print(const hash_table_t *ht)
 	printf("{");
 	for( i = 0; i < ht->size; i++)
 	{
-	        currNode = ht->array[i];
+		currNode = ht->array[i];
 		while(currNode)
 		{
-			printf("'%s': '%s'", currNode->key, currNode->value);
-			currNode = currNode->next;
-			if(currNode)
+			if(counter)
 				printf(", ");
+			counter += printf("'%s': '%s'", currNode->key,
+		        (currNode->value == NULL ? "(nill)" : currNode->value));
+			currNode = currNode->next;
 		}
 	}
-	printf("}");
+	printf("}\n");
 }
