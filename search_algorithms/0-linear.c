ad41e96de9b7f2f294e2de52ebfce72d2173d432
philip taylor <philip.dan.taylor@gmail.com>
2023-08-14 15:26:29 +0000

    betty fight

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,5 +1,13 @@
 #include "search_algos.h"
 
+/**
+ * linear_search - Search Array for Element value
+ * @array: Array to search
+ * @size: Size of array
+ * @value: Value to be found
+ *
+ * Return: Index of first Value or -1 
+*/
 int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
