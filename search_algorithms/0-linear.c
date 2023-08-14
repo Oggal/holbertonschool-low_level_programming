f4bcb78c77cc6a3834894385023151bf2c077d04
philip taylor <philip.dan.taylor@gmail.com>
2023-08-14 15:24:13 +0000

    betty fight

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -4,15 +4,15 @@ int linear_search(int *array, size_t size, int value)
 {
 	int i = 0;
 
-        if (array == NULL)
-                return (-1);
+	if (array == NULL)
+		return (-1);
 
-for (i = 0; i < size; i++)
-{
-        printf("Value checked array[%i] = [%i]", i, array[i]);
-        if (array[i] == value)
-                return (i);
-}
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%i] = [%i]", i, array[i]);
+		if (array[i] == value)
+			return (i);
+	}
 
-return (-1);
+	return (-1);
 }
