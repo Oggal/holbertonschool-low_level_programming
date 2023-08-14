c7bda81d29aa01327825b93d4e9ce48c1c0e900c
philip taylor <philip.dan.taylor@gmail.com>
2023-08-14 15:27:05 +0000

    betty fight

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -6,7 +6,7 @@
  * @size: Size of array
  * @value: Value to be found
  *
- * Return: Index of first Value or -1 
+ * Return: Index of first Value or -1
 */
 int linear_search(int *array, size_t size, int value)
 {
